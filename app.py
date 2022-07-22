477ae4da2c15b66eaaffd3d0431b8b3a3e0f8332
ankit <akopanda2241@gmail.com>
2022-07-22 11:33:44 +0530

    ready to commit

---
+++ app.py
@@ -0,0 +1,14 @@
+#                                              STRING
+print('wow man!')
+name='ankit'
+print('Hi',name,'\nHow are you')
+new=name.replace('n','s')
+print(new)
+
+#                                              NUMBER 
+print(64+65)
+nu='21'
+print('33'+nu)  #ERROR
+maxnum=[1,2,3,45,6,2,34,21]
+print(max(maxnum))
+print(bin(23))   #gives binary numer
